2b4c9ee51d3fea6b13e43f89f552a27bc4fd91a2
maybeliu <zui@user.local>
2016-08-01 18:04:49 +0800

    解决一个冲突

---
+++ Vendors/MBTabBar/Controller/BaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  BaseNavigationController.h
+//  jjb_user
+//
+//  Created by Maybe on 16/8/1.
+//  Copyright © 2016年 北京家家帮科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseNavigationController : UINavigationController
+
+@end
