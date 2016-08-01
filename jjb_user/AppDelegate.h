9bdfe80e8ad78c2280d94698e7b3f19ad548fdd6
Aimee <26608467@qq.com>
2016-08-01 11:32:19 +0800

    Initial Commit

---
+++ jjb_user/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jjb_user
+//
+//  Created by Aimee on 16/8/1.
+//  Copyright © 2016年 北京家家帮科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jjb_user/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jjb_user
+//
+//  Created by Aimee on 16/8/1.
+//  Copyright © 2016年 北京家家帮科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
