e8e01719de57e8317b3847a9676aa3dac8495b92
Check <yksrats@163.com>
2016-08-02 22:30:58 +0800

    创建详情页

--- jjb_user/Activity/Controller/ActivityListController.m
+++ jjb_user/Activity/Controller/ActivityListController.m
@@ -8,6 +8,7 @@
 
 #import "ActivityListController.h"
 #import "ActivityListCell.h"
+#import "ActivityDetailController.h"
 
 #define SIZE [UIScreen mainScreen].bounds.size
 
@@ -17,10 +18,13 @@
 
 @property (nonatomic,strong) NSMutableArray *dataArr;
 
+@property (nonatomic,strong) ActivityDetailController *detail;
+
 @end
 
 @implementation ActivityListController
 
+#pragma mark -- life cycle
 -(void)viewDidLoad
 {
     [super viewDidLoad];
@@ -32,7 +36,7 @@
     
     [self loadData];
     
-    [self createTableView];
+    [self.view addSubview:self.tableView];
 }
 
 
@@ -42,19 +46,6 @@
 
 - (void)loadData {
     
-}
-
-#pragma mark -- CreateTableView
-- (void)createTableView {
-    
-    self.tableView.frame = CGRectMake(0, 0, SIZE.width, SIZE.height-40);
-    
-    _tableView.delegate = self;
-    _tableView.dataSource = self;
-    
-    [_tableView registerClass:[ActivityListCell class] forCellReuseIdentifier:@"ID"];
-    
-    [self.view addSubview:self.tableView];
 }
 
 
@@ -67,9 +58,9 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    ActivityListCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ID" forIndexPath:indexPath];
+    ActivityListCell *cell = [tableView dequeueReusableCellWithIdentifier:@"List" forIndexPath:indexPath];
     if (cell == nil) {
-        cell = [[ActivityListCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"ID"];
+        cell = [[ActivityListCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"List"];
     }
     
     return cell;
@@ -80,12 +71,29 @@
     return 340;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+//    NSString *str=_dataDic[_array[indexPath.section]][indexPath.row];
+//    vc.name=str;
+
+//    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    self.detail.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:self.detail animated:YES];
+}
+
 #pragma mark -- getter and setter
 - (UITableView *)tableView {
     
     if (!_tableView) {
         
         _tableView = [[UITableView alloc] init];
+        _tableView.frame = CGRectMake(0, 0, SIZE.width, SIZE.height-40);
+        
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        
+        [_tableView registerClass:[ActivityListCell class] forCellReuseIdentifier:@"List"];
     }
     return _tableView;
 }
@@ -100,6 +108,14 @@
     
 }
 
+- (ActivityDetailController *)detail {
+
+    if (!_detail) {
+        
+        _detail = [[ActivityDetailController alloc] init];
+    }
+    return _detail;
+}
 
 
 
