f9de2c9056c1a9ebf38b90beda1a0952b28bfd15
Check <yksrats@163.com>
2016-08-03 18:12:15 +0800

    我的课程

--- jjb_user/My/Controller/MyCourseController.m
+++ jjb_user/My/Controller/MyCourseController.m
@@ -7,7 +7,84 @@
 //
 
 #import "MyCourseController.h"
+#import <Masonry.h>
+#import "MyCourseCell.h"
+
+@interface MyCourseController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic,strong) UITableView *tableView;
+
+@property (nonatomic,strong) NSMutableArray *array_course;
+
+@end
 
 @implementation MyCourseController
 
+#pragma -
+#pragma mark - life cycle
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"我的课程";
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    [self.view addSubview:self.tableView];
+    [self layoutPageSubviews];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma -
+#pragma mark - layoutPageSubviews
+- (void)layoutPageSubviews {
+    
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(SIZE.width, SIZE.height - 64 - 49));
+        make.top.left.equalTo(0);
+    }];
+}
+
+#pragma -
+#pragma mark - UITableViewDelegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    MyCourseCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyCourseCellIdentifier" forIndexPath:indexPath];
+    if (!cell) {
+        cell = [[MyCourseCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"MyCourseCellIdentifier"];
+    }
+    return cell;
+}
+    
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+        
+    return 210;
+}
+    
+#pragma -
+#pragma mark - getter and setter
+- (UITableView *)tableView {
+        
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] init];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        [_tableView registerClass:[MyCourseCell class] forCellReuseIdentifier:@"MyCourseCellIdentifier"];
+    }
+    return _tableView;
+}
+    
+- (NSMutableArray *)array_course{
+        
+    if (!_array_course) {
+        
+        _array_course = [NSMutableArray array];
+    }
+    return _array_course;
+}
 @end
