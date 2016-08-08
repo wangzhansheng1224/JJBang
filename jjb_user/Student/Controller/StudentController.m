8858e2040a1946ccbe41e611fadc945ea42e1b34
Check <yksrats@163.com>
2016-08-08 11:00:57 +0800

    明星学员首页

--- jjb_user/Student/Controller/StudentController.m
+++ jjb_user/Student/Controller/StudentController.m
@@ -7,7 +7,86 @@
 //
 
 #import "StudentController.h"
+#import "StudentListCell.h"
+
+static NSString  *const StudentListCellIdentifier=@"StudentListCellIdentifier";
+
+@interface StudentController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic,strong) UITableView *tableView;
+@property (nonatomic,strong) NSMutableArray *array_data;
+
+@end
 
 @implementation StudentController
+
+#pragma -
+#pragma mark - life cycle
+- (void)viewDidLoad {
+
+    [super viewDidLoad];
+    self.navigationController.navigationBarHidden = NO;
+    self.view.backgroundColor = COLOR_WHITE;
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    [self.view addSubview:self.tableView];
+    
+    [self layoutPageSubviews];
+}
+
+
+#pragma -
+#pragma mark - layoutPageSubviews
+- (void)layoutPageSubviews {
+
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.size.mas_equalTo(CGSizeMake(Screen_Width, Screen_Height));
+        make.top.equalTo(@0);
+        make.left.equalTo(@0);
+    }];
+}
+
+
+#pragma -
+#pragma mark - tableView delegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    StudentListCell *cell = [tableView dequeueReusableCellWithIdentifier:StudentListCellIdentifier forIndexPath:indexPath];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    return 94;
+}
+
+#pragma -
+#pragma mark - getters and setters
+- (UITableView *)tableView {
+
+    if (!_tableView) {
+        
+        _tableView = [[UITableView alloc] init];
+        _tableView.dataSource = self;
+        _tableView.delegate = self;
+        [_tableView registerClass:[StudentListCell class] forCellReuseIdentifier:StudentListCellIdentifier];
+    }
+    return _tableView;
+}
+
+- (NSMutableArray *)array_data {
+
+    if (!_array_data) {
+        
+        _array_data = [[NSMutableArray alloc] init];
+    }
+    return _array_data;
+}
 
 @end
