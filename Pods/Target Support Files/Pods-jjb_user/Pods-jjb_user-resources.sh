a00108697bcf7279e0ad30f33f7555bed76bef6c
Aimee <26608467@qq.com>
2016-08-11 10:47:45 +0800

    照片

--- Pods/Target Support Files/Pods-jjb_user/Pods-jjb_user-resources.sh
+++ Pods/Target Support Files/Pods-jjb_user/Pods-jjb_user-resources.sh
@@ -82,11 +82,13 @@ EOM
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "DateTools/DateTools/DateTools.bundle"
   install_resource "MJRefresh/MJRefresh/MJRefresh.bundle"
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/MWPhotoBrowser/MWPhotoBrowser.bundle"
   install_resource "XLForm/XLForm/XLForm.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "DateTools/DateTools/DateTools.bundle"
   install_resource "MJRefresh/MJRefresh/MJRefresh.bundle"
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/MWPhotoBrowser/MWPhotoBrowser.bundle"
   install_resource "XLForm/XLForm/XLForm.bundle"
 fi
 
