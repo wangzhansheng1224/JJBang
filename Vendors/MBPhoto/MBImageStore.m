3824a43b55724eac6cf550323dda7fe4306802f8
xiaoyinghun <gutongwuyu@163.com>
2016-09-01 15:33:39 +0800

    333

---
+++ Vendors/MBPhoto/MBImageStore.m
@@ -0,0 +1,84 @@
+//
+//  MBImageStore.m
+//  jjb_user
+//
+//  Created by Maybe on 16/9/1.
+//  Copyright © 2016年 北京家家帮科技有限公司. All rights reserved.
+//
+
+#import "MBImageStore.h"
+
+@interface MBImageStore ()
+@property(nonatomic,strong)NSMutableDictionary * dictionary;
+
+@end
+
+@implementation MBImageStore
+SingleM(MBImageStore)
+
+#pragma -
+#pragma mark - life cycle
+-(instancetype)init
+{
+    self = [super init];
+    if (self) {
+    //注册为低内存通知的观察者
+        NSNotificationCenter * nc = [NSNotificationCenter defaultCenter];
+        [nc addObserver:self selector:@selector(clearCaches:) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
+        
+    }
+    return self;
+}
+
+#pragma -
+#pragma - private methods
+-(void)setImage:(UIImage *)image forKey:(NSString *)key
+{
+    [self.dictionary setObject:image forKey:key];
+    //获取图片全路径
+    NSString * path = [self imagePathForKey:key];
+    //从图片中提取JPEG格式的数据，0.5f是图片压缩参数
+    NSData * data = UIImageJPEGRepresentation(image, 0.5f);
+    [data writeToFile:path atomically:YES];
+}
+-(UIImage *)imageForKey:(NSString *)key
+{
+    UIImage * image = [self.dictionary objectForKey:key];
+    if (!image) {
+        NSString * path = [self imagePathForKey:key];
+        image = [UIImage imageWithContentsOfFile:path];
+        if (image) {
+            [self.dictionary setObject:image forKey:key];
+        }
+        else
+        {
+            NSLog(@"图片不存在");
+            [self imagePathForKey:key];
+        }
+    }
+    return image;
+}
+
+-(NSString *)imagePathForKey:(NSString *)key
+{
+    NSArray * documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString * documentDirectory = [documentDirectories firstObject];
+    return [documentDirectory stringByAppendingString:key];
+}
+
+-(void)clearCaches:(NSNotificationCenter *)nfc
+{
+    NSLog(@"摄像头报警了%lu",(unsigned long)self.dictionary.count);
+    [self.dictionary removeAllObjects];
+}
+#pragma -
+#pragma mark - getter and setter
+-(NSMutableDictionary *)dictionary
+{
+    if (_dictionary ==nil) {
+        _dictionary = [NSMutableDictionary dictionary];
+    }
+    return _dictionary;
+}
+
+@end
--- jjb_user/My/Controller/ChangeHeaderIconController.m
+++ jjb_user/My/Controller/ChangeHeaderIconController.m
@@ -7,17 +7,27 @@
 //
 
 #import "ChangeHeaderIconController.h"
-
-@interface ChangeHeaderIconController ()
+#import "changeMyInfoAPIManager.h"
+#import "MBImageStore.h"
+#import "OSSManager.h"
+#import "ImgModel.h"
+@interface ChangeHeaderIconController ()<LDAPIManagerApiCallBackDelegate,LDAPIManagerParamSourceDelegate,UIImagePickerControllerDelegate,UIPopoverControllerDelegate,UINavigationControllerDelegate>
+@property(nonatomic,strong)UIPopoverController * imagePickerPopver;
+@property(nonatomic,strong) UIImageView * headImageView;
+@property(nonatomic,strong) LDAPIBaseManager * changeHeaderAPIManager;
 
 @end
 
 @implementation ChangeHeaderIconController
 
+#pragma -
+#pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = COLOR_WHITE;
-    self.navigationItem.title = @"修改头像";
+    self.view.backgroundColor = [UIColor blackColor];
+
+    [self setUpNav];
+    [self setChildViewContraints];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,14 +35,148 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)setUpNav
+{
+    self.navigationItem.title = @"修改头像";
+    UIBarButtonItem * chooseBtn = [UIBarButtonItem itmeWithNormalImage:nil high:nil target:self action:@selector(chooseButtonClick:) norColor:nil highColor:nil title:@"这里缺张图片"];
+    self.navigationItem.rightBarButtonItem = chooseBtn;
+    
+    
 }
-*/
+#pragma -
+#pragma - SubViewContriaints
+-(void)setChildViewContraints
+{
+    [self.headImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top).offset(80);
+        make.left.equalTo(self.view.mas_left);
+        make.right.equalTo(self.view.mas_right);
+        make.bottom.equalTo(self.view.mas_bottom).offset(-80);
+    }];
+    
+    
+}
+
+
+
+#pragma -
+#pragma mark - buttonClick event
+-(void)chooseButtonClick:(id)sender
+{
+    if ([self.imagePickerPopver isPopoverVisible]) {
+        [self.imagePickerPopver dismissPopoverAnimated:YES];
+        self.imagePickerPopver = nil;
+        return;
+    }
+    UIImagePickerController * imagePicker = [[UIImagePickerController alloc]init];
+    imagePicker.editing = YES;
+    imagePicker.delegate = self;
+    //允许编辑图片
+    imagePicker.allowsEditing = YES;
+    UIAlertController * alertView = [UIAlertController alertControllerWithTitle:@"请选择打开方式" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    [alertView addAction:[UIAlertAction actionWithTitle:@"照相机" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+            self.imagePickerPopver = [[UIPopoverController alloc]initWithContentViewController:imagePicker];
+            self.imagePickerPopver.delegate = self;
+            [self.imagePickerPopver presentPopoverFromBarButtonItem:sender permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+        }
+        else
+        {
+            [self presentViewController:imagePicker animated:YES
+                             completion:nil];
+        }
+    }] ];
+    
+    [alertView addAction:[UIAlertAction actionWithTitle:@"相册" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+            self.imagePickerPopver = [[UIPopoverController alloc]initWithContentViewController:imagePicker];
+            self.imagePickerPopver.delegate = self;
+            [self.imagePickerPopver presentPopoverFromBarButtonItem:sender permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+        }
+        else
+        {
+            [self presentViewController:imagePicker animated:YES completion:nil];
+        }
+        
+    }]];
+    [alertView addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+    }]];
+    [self presentViewController:alertView animated:YES
+                     completion:nil];
+    
+}
+
+#pragma -
+#pragma mark - UIImagePickerControllerDelegate
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    UIImage * image = [info valueForKey:UIImagePickerControllerEditedImage];
+    [[MBImageStore shareMBImageStore] setImage:image forKey:@"MBStore"];
+    self.headImageView.image = image;
+    UIImageWriteToSavedPhotosAlbum(image, self, nil, nil);
+    
+    NSData * editImageData = UIImageJPEGRepresentation(image, 0.8f);
+   NSString * name =  [NSString stringWithFormat:@"%@.jpg",[[OSSManager shareInstance]currentTimeByJava]];
+//    ImgModel * model = [[ImgModel alloc]init];
+    [self.view makeToast:@"正在上传" duration:1.0f position:CSToastPositionCenter];
+    [[OSSManager shareInstance]uploadFiles:@[image] withTargetSubPath:OSSMessagePath withBlock:^{
+        [self.view makeToast:@"上传成功" duration:1.0f position:CSToastPositionCenter];
+        [self.changeHeaderAPIManager loadData];
+    }];
+    
+    
+    
+    if (self.imagePickerPopver) {
+        [self.imagePickerPopver dismissPopoverAnimated:YES];
+        self.imagePickerPopver = nil;
+    }
+    else
+    {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+    
+}
+
+#pragma -
+#pragma mark - LDAPIManagerApiCallBackDelegate
+- (void)apiManagerCallDidSuccess:(LDAPIBaseManager *)manager{
+    
+    NSDictionary *resultData = [manager fetchDataWithReformer:nil];
+    NSDictionary * dict  = resultData[@"data"];
+
+    
+}
+
+- (void)apiManagerCallDidFailed:(LDAPIBaseManager *)manager{
+    
+}
+
+- (NSDictionary *)paramsForApi:(LDAPIBaseManager *)manager
+{
+    
+    return @{
+             @"user_id" :@([UserModel currentUser].userID),
+             @"photo":@""
+             
+             };
+}
+
+#pragma -
+#pragma mark - getter and setter
+-(UIImageView *)headImageView
+{
+    if (_headImageView == nil) {
+        UIImageView * imageView = [[UIImageView alloc]init];
+        
+        imageView.image = [UIImage imageNamed:[UserModel currentUser].photo];
+        [self.view addSubview:imageView];
+        _headImageView = imageView;
+    }
+    return _headImageView;
+}
+
 
 @end
